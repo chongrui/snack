6ab93cedf805bde4ee4b02fed4df44711ea7711e
Nick <hanzhang.zhu@hds.com>
2016-07-14 15:45:24 -0700

    Replaced old starter app with a new template

--- www/js/app.js
+++ www/js/app.js
@@ -1,85 +1,171 @@
-// Ionic Starter App
+/*
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+  DeepBlue Starter Kit - version 1.1
+  Copyright (c) 2015 INMAGIK SRL - www.inmagik.com
+  All rights reserved
 
-.run(function($ionicPlatform) {
+  written by Mauro Bianchi
+  bianchimro@gmail.com  
+  
+  file: app.js
+  
+*/
+
+angular.module('deepBlue', ['ionic', 'deepBlue.controllers', 'deepBlue.services'])
+
+.run(function($ionicPlatform, $rootScope, $timeout, $state) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+
+    /* 
+      #SIMPLIFIED-IMPLEMENTATION:
+      Example access control.
+      A real app would probably call a service method to check if there
+      is a logged user.
+
+      #IMPLEMENTATION-DETAIL: views that require authorizations have an
+      "auth" key with value = "true".
+    */
+    $rootScope.$on('$stateChangeStart', 
+      function(event, toState, toParams, fromState, fromParams){
+        if(toState.data && toState.data.auth == true && !$rootScope.user.email){
+          event.preventDefault();
+          $state.go('app.login');   
+        }
+    });
+
   });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+  /*
+
+    Here we setup the views of our app.
+    In this case:
+    - feed, account, shop, checkout, cart will require login
+    - app will go to the "start view" when launched.
+
+    #IMPLEMENTATION-DETAIL: views that require authorizations have an
+    "auth" key with value = "true".
+
+  */
+  
   $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
+  .state('app', {
+    url: '/app',
     abstract: true,
-    templateUrl: 'templates/tabs.html'
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
   })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
+  
+  .state('app.start', {
+    url: '/start',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'menuContent': {
+        templateUrl: 'templates/start.html'
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
+  .state('app.login', {
+    url: '/login',
+    cached : false,
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'menuContent': {
+        templateUrl: 'templates/login.html',
+        controller : 'LoginCtrl'
       }
     }
-  });
+  })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  .state('app.forgot', {
+    url: '/forgot',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/forgot.html'
+      }
+    }
+  })
+
+  .state('app.signup', {
+    url: '/signup',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/signup.html'
+      }
+    }
+  })
+
+  .state('app.account', {
+      url: '/account',
+      data : { auth : true },
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/account.html',
+          controller : 'AccountCtrl'
+        }
+      }
+  })
+
+  .state('app.feed', {
+    url: '/feed',
+    data : { auth : true },
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/feed.html',
+        controller : 'FeedsCtrl'
+      }
+    }
+  })
+
+  .state('app.shop', {
+    url: '/shop',
+    data : { auth : true },
+    cache : false,
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/feed.html',
+        controller : 'FeedsCtrl'
+      }
+    }
+  })
+
+  .state('app.cart', {
+    url: '/cart',
+    data : { auth : true },
+    cache : false,
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/cart.html',
+        controller : 'CartCtrl'
+      }
+    }
+  })
+
+  .state('app.checkout', {
+    url: '/checkout',
+    data : { auth : true },
+    cache : false,
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/checkout.html',
+        controller : 'CheckoutCtrl'
+      }
+    }
+  })
+  
+  // If none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/start');
 
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,81 @@
-angular.module('starter.services', [])
+/*
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+  DeepBlue Starter Kit - version 1.1
+  Copyright (c) 2015 INMAGIK SRL - www.inmagik.com
+  All rights reserved
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+  written by Mauro Bianchi
+  bianchimro@gmail.com  
+  
+  file: services.js
+  description: this file contains all services of the DeepBlue app.
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
+*/
+
+
+angular.module('deepBlue.services', [])
+
+// CartService is an example of service using localStorage 
+// to persist items of the cart.
+.factory('CartService', [function () {
+
+  var svc = {};
+
+  svc.saveCart = function(cart){
+    window.localStorage.setItem('cart', JSON.stringify(cart));
   };
-});
+
+  svc.loadCart = function(){
+    var cart = window.localStorage.getItem('cart');
+    if(!cart){
+      return { products : [ ] }
+    }
+    return JSON.parse(cart);
+  };
+
+  svc.resetCart = function(){
+    var cart =  { products : [ ] };
+    svc.saveCart(cart);
+    return cart;
+  };
+
+  svc.getTotal = function(cart){
+    var out = 0;
+    if(!cart || !cart.products || !angular.isArray(cart.products)){
+      return out;
+    }
+    for(var i=0; i < cart.products.length; i++){
+      out += cart.products[i].price;
+    }
+    return out;
+  }
+
+  return svc;
+
+}])
+
+// #SIMPLIFIED-IMPLEMENTATION
+// This is an example if backend service using $http to get
+// data from files.
+// In this example, files are shipped with the application, so 
+// they are static and cannot change unless you deploy an application update
+// Other possible implementations (not covered by this kit) include:
+// - loading dynamically json files from the web 
+// - calling a web service to fetch data dinamically
+// in those cases be sure to handle url whitelisting (specially in android)
+// (https://cordova.apache.org/docs/en/5.0.0/guide_appdev_whitelist_index.md.html)
+// and handle network errors in your interface
+.factory('BackendService', ['$http', function ($http) {
+
+  var svc = {};
+
+  svc.getFeeds = function(){
+    return $http.get('sampledata/feeds.json');
+  }
+
+  svc.getProducts = function(){
+    return $http.get('sampledata/products.json');
+  }
+
+  return svc;
+}])
